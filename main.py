ffb8484a0d18f19659905d4e49d9670361d687f0
emilypnascimento <emilypdonascimento@gmail.com>
2026-03-08 16:53:02 -0300

    Final implementation

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("greetings boots")
+from stats import word_count, char_count, sorted_list
+import sys
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    content = get_book_text(book_path)
+    num_words = word_count(content)
+    chars = char_count(content)
+    char_count_list = sorted_list(chars)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_count_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
+main()
